81b5cd5923708b3abc14c4922377c646e2b17319
Rafał Gosiewski <rafal.gosiewski@gmail.com>
2022-05-05 20:39:21 +0200

    Configure log4j logging to console, file and TCP

--- src/main/java/com/rgosiewski/psobczyk/pk/elk/ElkApplication.java
+++ src/main/java/com/rgosiewski/psobczyk/pk/elk/ElkApplication.java
@@ -1,59 +1,13 @@
 package com.rgosiewski.psobczyk.pk.elk;
 
-import com.rgosiewski.psobczyk.pk.elk.logging.enums.Severity;
-import com.rgosiewski.psobczyk.pk.elk.logging.service.LoggingService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ElkApplication {
-    private final static Logger logger = LoggerFactory.getLogger(LoggingService.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ElkApplication.class, args);
-
-        generateLogs();
     }
 
-    public static void generateLogs() {
-        for (int order = 0; order < 200; order++) {
-            Severity severity = Severity.byOrder(order % 4);
-            log(severity, order);
-            try {
-                Thread.sleep(order % 100);
-            } catch (InterruptedException e) {
-                logger.error("Sleep interrupted for {}", order, e);
-            }
-        }
-    }
-
-    private static void log(Severity severity, int order) {
-        if (Severity.INFO.equals(severity)) {
-            info(order);
-        } else if (severity.equals(Severity.WARN)) {
-            warn(order);
-        } else if (severity.equals(Severity.ERROR)) {
-            error(order);
-        } else if (severity.equals(Severity.DEBUG)) {
-            debug(order);
-        }
-    }
-
-    private static void info(int order) {
-        logger.info("Info log {}", order);
-    }
-
-    private static void warn(int order) {
-        logger.warn("Warn log {}", order);
-    }
-
-    private static void error(int order) {
-        logger.error("Error log {}", order);
-    }
-
-    private static void debug(int order) {
-        logger.debug("Debug log {}", order);
-    }
 }
--- src/main/java/com/rgosiewski/psobczyk/pk/elk/logging/service/LoggingService.java
+++ src/main/java/com/rgosiewski/psobczyk/pk/elk/logging/service/LoggingService.java
@@ -1,16 +1,17 @@
 package com.rgosiewski.psobczyk.pk.elk.logging.service;
 
 import com.rgosiewski.psobczyk.pk.elk.logging.enums.Severity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoggingService {
-    private final static Logger logger = LoggerFactory.getLogger(LoggingService.class);
+    private final static Logger logger = LogManager.getLogger(LoggingService.class);
+    private final static Integer LOG_COUNT = 250;
 
     public void generateLogs() {
-        for (int order = 0; order < 1000; order++) {
+        for (int order = 0; order < LOG_COUNT; order++) {
             Severity severity = Severity.byOrder(order % 4);
             log(severity, order);
             try {
